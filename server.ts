8df7d76e72fb8f56c5651ae60e3e421d5f4d12c6
truong264vu <xuantruongvu264@gmail.com>
2024-01-17 11:07:54 +0700

    .

--- server.ts
+++ server.ts
@@ -6,12 +6,12 @@ const server: Server = new Server(app);
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
 
 app
-  .listen(PORT, "localhost", function () {
+  .listen(PORT, "0.0.0.0", function () {
     console.log(`Server is running on port ${PORT}.`);
   })
   .on("error", (err: any) => {
     if (err.code === "EADDRINUSE") {
-      console.log("Error: address already in use");
+      console.log("Error: Address already in use");
     } else {
       console.log(err);
     }
